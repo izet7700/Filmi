7d282c74a02d3e8db5b9eaf4860f9ca969d61413
izet7700 <43537592+izet7700@users.noreply.github.com>
2018-10-08 12:01:23 +0200

    prvi commit

---
+++ filmi/MainForm.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace filmi
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imeTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.priimekTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.passMaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.telefonTextBox = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.email2TextBox = new System.Windows.Forms.TextBox();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pass2MaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.krajComboBox = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // imeTextBox
+            // 
+            this.imeTextBox.Location = new System.Drawing.Point(139, 13);
+            this.imeTextBox.Name = "imeTextBox";
+            this.imeTextBox.Size = new System.Drawing.Size(180, 20);
+            this.imeTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ime:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Priimek:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "e-mail:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "password:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Datum rojstva:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "telefon:";
+            // 
+            // priimekTextBox
+            // 
+            this.priimekTextBox.Location = new System.Drawing.Point(139, 48);
+            this.priimekTextBox.Name = "priimekTextBox";
+            this.priimekTextBox.Size = new System.Drawing.Size(180, 20);
+            this.priimekTextBox.TabIndex = 7;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(139, 79);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(180, 20);
+            this.emailTextBox.TabIndex = 8;
+            // 
+            // passMaskedTextBox
+            // 
+            this.passMaskedTextBox.Location = new System.Drawing.Point(139, 114);
+            this.passMaskedTextBox.Name = "passMaskedTextBox";
+            this.passMaskedTextBox.Size = new System.Drawing.Size(180, 20);
+            this.passMaskedTextBox.TabIndex = 9;
+            // 
+            // telefonTextBox
+            // 
+            this.telefonTextBox.Location = new System.Drawing.Point(139, 171);
+            this.telefonTextBox.Name = "telefonTextBox";
+            this.telefonTextBox.Size = new System.Drawing.Size(180, 20);
+            this.telefonTextBox.TabIndex = 11;
+            this.telefonTextBox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(139, 141);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(180, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(153, 236);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 24);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Login";
+            // 
+            // email2TextBox
+            // 
+            this.email2TextBox.Location = new System.Drawing.Point(139, 273);
+            this.email2TextBox.Name = "email2TextBox";
+            this.email2TextBox.Size = new System.Drawing.Size(180, 20);
+            this.email2TextBox.TabIndex = 14;
+            // 
+            // registerButton
+            // 
+            this.registerButton.Location = new System.Drawing.Point(244, 225);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(75, 23);
+            this.registerButton.TabIndex = 16;
+            this.registerButton.Text = "Register";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(244, 327);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 17;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 276);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "e-mail:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 304);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Password:";
+            // 
+            // pass2MaskedTextBox
+            // 
+            this.pass2MaskedTextBox.Location = new System.Drawing.Point(139, 301);
+            this.pass2MaskedTextBox.Name = "pass2MaskedTextBox";
+            this.pass2MaskedTextBox.Size = new System.Drawing.Size(180, 20);
+            this.pass2MaskedTextBox.TabIndex = 20;
+            // 
+            // krajComboBox
+            // 
+            this.krajComboBox.FormattingEnabled = true;
+            this.krajComboBox.Location = new System.Drawing.Point(139, 198);
+            this.krajComboBox.Name = "krajComboBox";
+            this.krajComboBox.Size = new System.Drawing.Size(180, 21);
+            this.krajComboBox.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(13, 201);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(27, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "kraj:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 375);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.krajComboBox);
+            this.Controls.Add(this.pass2MaskedTextBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.email2TextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.telefonTextBox);
+            this.Controls.Add(this.passMaskedTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.priimekTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imeTextBox);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox imeTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox priimekTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.MaskedTextBox passMaskedTextBox;
+        private System.Windows.Forms.TextBox telefonTextBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox email2TextBox;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.MaskedTextBox pass2MaskedTextBox;
+        private System.Windows.Forms.ComboBox krajComboBox;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ filmi/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace filmi
+{
+   public partial class MainForm : Form
+    {
+        string ime, priimek, email, password, telefon, kraj;
+        public MainForm()
+        {
+            string MyConString = "SERVER=den1.mysql2.gear.host;" +
+            "DATABASE=filmi;" +
+            "UID=filmi;" +
+            "PASSWORD=izet.m;";
+            MySqlConnection connection = new MySqlConnection(MyConString);
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+            ime = imeTextBox.Text;
+            priimek = priimekTextBox.Text;
+            email = emailTextBox.Text;
+            password = passMaskedTextBox.Text;
+            kraj = krajComboBox.ValueMember;
+            DateTime datumroj = dateTimePicker1.Value;
+            string datum = datumroj.ToString("yyyy-MM-dd");
+            telefon = telefonTextBox.Text;
+            if (ime == "" || priimek == "" || email == "" || kraj=="")
+            {
+                MessageBox.Show("Not valid values for ime/priimek/email");
+            }
+            else
+            {
+                string MyConString = "SERVER=den1.mysql2.gear.host;" +
+                "DATABASE=filmi;" +
+                "UID=filmi;" +
+                "PASSWORD=izet.m;";
+                MySqlConnection connection = new MySqlConnection(MyConString);
+                connection.Open();
+                string register="INSERT INTO filmi.uporabniki(ime,priimek,kraj_id,email,password,telefon," +
+                    "datum_roj,vrsta_uporabnika) VALUES ('"+ ime +"','"+ priimek +"', '(SELECT id FROM" +
+                    "filmi.kraji WHERE ime='" + kraj+ "')','" + email + "','')";
+
+            }
+        }
+    }
+}
